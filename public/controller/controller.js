7d31a90c9c689249197b1b25a194a88877f102f4
dattaprasad <dattaprasad.mundada@pubmatic.com>
2016-01-20 12:53:18 +0530

    First Commit

---
+++ public/controller/controller.js
@@ -0,0 +1,42 @@
+function AppCtrl($scope, $http) {
+	console.log("Hello World from Controller");
+
+	var refresh =function() {
+		$http.get('/contactlist').success(function(response) {
+			console.log("I got the data I requested");
+			$scope.contactlist = response;
+		});
+	};
+	refresh();
+	
+	$scope.addContact = function() {
+		$http.post('/contactlist',$scope.contact).success(function(response){
+			console.log(response);
+			refresh();
+		});
+	};	
+
+    $scope.remove =function(id) {
+             console.log(id);
+             $http.delete('/contactlist/'+ id).success(function(response){
+				refresh();
+			});
+	};
+
+	$scope.edit =function(id) {
+		console.log(id);
+		$http.get('/contactlist/'+ id).success(function(response) {
+			$scope.contact = response;
+		});
+	};
+
+	$scope.update =function() {
+		console.log($scope.contact._id);
+		$http.put('/contactlist/'+$scope.contact._id, $scope.contact).success(function(response){
+			refresh();
+		});
+	};
+	$scope.deSelect = function(){
+		$scope.contact="";
+	};
+}
